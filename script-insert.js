7cc704733ddea82a0a1204f9160592c6d824649b
vestaddbox@gmail.com <vestaddbox@gmail.com>
2022-08-08 19:19:23 +0100

    hj

--- script-insert.js
+++ script-insert.js
@@ -1,5 +1,5 @@
 const form = document.querySelector("#form")
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbwqu2btfSHvJvk-ZBpGoNC1A4u557Wiy3eYNfuv_Rb1UBXUDDgMLI81urCxAR3SnK5v/exec'
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbx400-OITHD696EM-oXbbKIR-wBQnInYb3mjze4gs7gABMFvQCp-KR01Xh_sKAh5Q3g9A/exec'
 
     form.addEventListener('submit', e => {
       e.preventDefault()
